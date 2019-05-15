5097ca5cfbc1aecb3caccb032fabc025b9e1180c
darylbre <roderickbreland@gmail.com>
2019-05-15 10:14:05 -0400

    New Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,134 +1,206 @@
+let score = 0;
+let userAnswer;
+let wrong = 0;
+let timer;
 const myQuestions = [
     {
-        question: "Who is the artist that sang What's going on?",
-        answers: {
-            a: "Stevie Wonder",
-            b: "Smokey Robinson",
-            c: "Marvin Gaye"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which instrument is a woodwind?",
-        answers: {
-            a: "Trumpet",
-            b: "Tuba",
-            c: "Clarinet"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Where is James Brown from?",
-        answers: {
-            a: "Savannah",
-            b: "Atlanta",
-            c: "Macon",
-            d: "Augusta"
-        },
-        correctAnswer: "d"
+        question: "1. Who recorded the song &quot;What's Going On&quot;?",
+        option1: "Stevie Wonder",
+        option2: "Smokey Robinson",
+        option3: "Marvin Gaye",
+        option4: "David Ruffin",
+        correctOption: "Marvin Gaye"
     }
-];
 
-function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
-   
-    // for each question...
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
+    ,
+    {
+        question: "2. Which instrument is a woodwind?",
+        option1: "Trumpet",
+        option2: "Tuba",
+        option3: "Clarinet",
+        option4: "Trombone",
+        correctOption: "Clarinet"
+    }
+    ,
+    {
+        question: "3. What city is James Brown from?",
+        option1: "Savannah",
+        option2: "Atlanta",
+        option3: "Macon",
+        option4: "Augusta",
+        correctOption: "Augusta"
+    }
+    ,
+    {
+        question: "4. Who recorded the original version of the song &quot;Hound Dog&quot;?",
+        option1: "Willa Mae Big Mama Thornton",
+        option2: "Elvis Presley",
+        option3: "Carl Perkins",
+        option4: "Chuck Berry",
+        correctOption: "Willa Mae Big Mama Thornton"
+    }
+    ,
+    {
+        question: "5. Steward Copeland was the drummer for which band?",
+        option1: "The Who",
+        option2: "The Doors",
+        option3: "The Police",
+        option4: "Rolling Stones",
+        correctOption: "The Police"
+    }
+    ,
+    {
+        question: "6. The Stratocaster is a model of which guitar maker?",
+        option1: "Fender",
+        option2: "Ibanez",
+        option3: "Martin",
+        option4: "ESP",
+        correctOption: "Fender"
+    }
+    ,
+    {
+        question: "7. Whose childhood hit was Fingertips?",
+        option1: "Marvin Gaye",
+        option2: "Stevie Wonder",
+        option3: "Smokey Robinson",
+        option4: "Chuck Berry",
+        correctOption: "Stevie Wonder"
+    }
+    ,
+    {
+        question: "8. What was Madonna’s first UK top ten single?",
+        option1: "Like a Virgin",
+        option2: "Everybody",
+        option3: "Like a Prayer",
+        option4: "Holiday",
+        correctOption: "Holiday"
+    }
+    ,
+    {
+        question: "9. Who recorded the original version of the song &quot;Johnny B Goode &quot;?",
+        option1: "Willa Mae Big Mama Thornton",
+        option2: "Elvis Presley",
+        option3: "Carl Perkins",
+        option4: "Chuck Berry",
+        correctOption: "Chuck Berry"
+    }
+    ,
+    {
+        question: "10. When was Chess Records founded?",
+        option1: "1920",
+        option2: "1965",
+        option3: "1948",
+        option4: "1950",
+        correctOption: "1950"
 
-            // we'll want to store the list of answer choices
-            const answers = [];
+    }];
 
-            // and for each available answer...
-            for (letter in currentQuestion.answers) {
 
-                // ...add an HTML radio button
-                answers.push(
-                    `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-                );
-            }
+function getValue(e) {
+    e.preventDefault();
+    const value = $('input[name="radio"]:checked').val();
+    console.log(value);
+    userAnswer = value;
+    checkAnswer();
+}
 
-            // add this question and its answers to the output
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-            );
+let countDown = 10;
+let currQuestion = 0;
+
+$(document).on("submit", "#questionForm", getValue);
+
+function startGame() {
+    // setInterval(displayCountDown, 1000);
+    displaymyQuestion();
+    currQuestion = 0;
+    setInterval(displaymyQuestion, 10000);
+}
+
+// nextQuestQueion function to reset counter, increase currQuestion, call displaymyQuestion()
+function nextQuestion() {
+    countDown = 10;
+   clearInterval(timer);
+    currQuestion++;
+    displaymyQuestion();
+
+
+}
+
+function displaymyQuestion() {
+    // timer = setInterval(displayCountDown, 1000)
+    timer =setInterval(displayCountDown, 1000);
+    const questionDiv = `
+    <div>
+    <h3 class="question">${myQuestions[currQuestion].question}</h3>
+    <form id="questionForm">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="radio" id="radio1" value="${myQuestions[currQuestion].option1}">
+            <label class="form-check-label" for="radio1">
+            ${myQuestions[currQuestion].option1}
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="radio" id="radio2" value="${myQuestions[currQuestion].option2}">
+            <label class="form-check-label" for="radio2">
+                ${myQuestions[currQuestion].option2}
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="radio" id="radio3" value="${myQuestions[currQuestion].option3}">
+            <label class="form-check-label" for="radio2">
+                ${myQuestions[currQuestion].option3}
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="radio" id="radio4" value="${myQuestions[currQuestion].option4}">
+            <label class="form-check-label" for="radio2">
+                ${myQuestions[currQuestion].option4}
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+    `
+
+    $("#root").html(questionDiv);
+    setTimeout(nextQuestion, 10000);
+}
+
+function displayCountDown() {
+    $("#countdown").text(countDown);
+    countDown--;
+    // if time runs out: wrong++, next question
+    // clearInterval(timer)
+    if (countDown === 0) {
+        wrong++;
+    }
+}
+function checkAnswer() {
+    console.log('User Answer: ', userAnswer);
+    const correctAnswer = myQuestions[currQuestion].correctOption;
+    console.log('Correct Answer: ', correctAnswer)
+    if (userAnswer === correctAnswer) {
+        score++;
+        alert("Right");
+    } else {
+        alert("Wrong");
+    }
+    currQuestion++;
+    nextQuestion()
+    if (currQuestion === myQuestions.length - 1) // game over function currQuestion = 0;
+        countDown = 10;
+    function gameOver() {
+        if (currQuestion === 0) {
+            gameOver;
+            alert("You got" + score + "/" + myQuestions.length);
         }
-    );
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-}
-
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-// function buildQuiz() { }
-
-function showResults() { }
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-const previousButton = document.getElementById("previous");
-const nextButton = document.getElementById("next");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-
-function showSlide(n) {
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-    if (currentSlide === 0) {
-        previousButton.style.display = 'none';
-    }
-    else {
-        previousButton.style.display = 'inline-block';
-    }
-    if (currentSlide === slides.length - 1) {
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
-    }
-    else {
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
     }
 }
-showSlide(0);
-function showNextSlide() {
-    showSlide(currentSlide + 1);
-}
+// alert("You got" + score + "/" + myQuestions.length);
 
-function showPreviousSlide() {
-    showSlide(currentSlide - 1);
-}
-
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
-
-function updateClock(){
-    var t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + '<br>' +
-                      'hours: '+ t.hours + '<br>' +
-                      'minutes: ' + t.minutes + '<br>' +
-                      'seconds: ' + t.seconds;
-    if(t.total<=0){
-      clearInterval(timeinterval);
-    }
-  }
-  
-  updateClock(); // run function once at first to avoid delay
-  var timeinterval = setInterval(updateClock,1000);
-  var daysSpan = clock.querySelector('.days');
-var hoursSpan = clock.querySelector('.hours');
-var minutesSpan = clock.querySelector('.minutes');
-var secondsSpan = clock.querySelector('.seconds');
+// displayCountDown();
+startGame();
+// setInterval(displaymyQuestions, 10000);
+// setInterval(displayCountDown, 1000);
 
 
